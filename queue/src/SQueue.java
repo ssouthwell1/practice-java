54e1c8b6aa85f781c7a1dcd620f38385bdad87a4
Shamari Southwell <shamari25southwell@gmail.com>
2018-06-29 01:43:25 -0400

    added queue functionality using 2 stacks

--- queue/src/SQueue.java
+++ queue/src/SQueue.java
@@ -1,19 +1,34 @@
+import java.util.Stack;
+
 public class SQueue {
 
+    Stack stack1;
+    Stack stack2;
+
+    // Queue - First in first out data structure (FIFO)
+    // Stack - Last in first out data structure
+
+    /**
+     * Here the objective is to implement a queue data structure with 2 stacks -- see enqueue and dequeue methods
+     */
 
     public SQueue() {
 
     }
 
     public void enqueue(int value) {
-
+        stack1.push(value);
     }
 
-    public void dequeue() {
-
-
+    public int dequeue() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        int x = (int) stack2.pop();
+        return x;
     }
-
 
 
 }
